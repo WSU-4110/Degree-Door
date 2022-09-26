2276be73c1e14e5aae98ea109ed1f70431ccd593
Patrick <patrickducusin2@gmail.com>
2022-09-25 21:39:12 -0400

    Create a handleChange function to keep track of changes to form data state.

--- pages/register.js
+++ pages/register.js
@@ -13,6 +13,18 @@ export default function Register() {
     }
   )
 
+  // In React, create a handleChange function
+  // for the data of the form fields.
+  function handleChange(event) {
+    const { name, value, type} = event.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value // ES6 computed name syntax for field
+      }
+    })
+  }
+
   return (
     <div>Register</div>
   )
