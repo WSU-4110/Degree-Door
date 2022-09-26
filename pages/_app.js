b791f4819ef6f78dc9e3b43d8d67e1a20021d385
Patrick <patrickducusin2@gmail.com>
2022-09-25 22:32:27 -0400

    Revert _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import '../styles/globals.css'
-import Register from './register'
 
 function MyApp({ Component, pageProps }) {
-  if(true) {
-    return <Register/>
-  }
   return <Component {...pageProps} />
 }
 
