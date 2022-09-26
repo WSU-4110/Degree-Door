d84474fea8f0332b8f9d06a72e2734e0287c525d
Patrick <patrickducusin2@gmail.com>
2022-09-25 21:34:15 -0400

    Create a form data state to register page.

--- pages/register.js
+++ pages/register.js
@@ -1,4 +1,15 @@
 export default function Register() {
+  const [formData, setFormData] = useState(
+    {
+      firstName: "",
+      lastName: "",
+      status: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    }
+  )
+  
   return (
     <div>Register</div>
   )
