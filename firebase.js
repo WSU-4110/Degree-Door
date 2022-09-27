10db157a483486b7ae2fa74079e69f96c209862f
PattyCoding123 <patrickducusin2@gmail.com>
2022-09-27 18:11:28 -0400

    Setup firebase.js configuration file

---
+++ firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize a new Firebase app instance if there were no 
+// previous instances.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Access the app's Firestore instance.
+const db = getFirestore(app);
+
+// Access the app's authentication instance.
+const auth = getAuth(app);
+
+// Export the db and auth variables.
+export { db, auth };
