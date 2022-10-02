055eadaf9dea88ac1f95f0934973508402da2bfc
Patrick <patrickducusin2@gmail.com>
2022-10-01 22:22:49 -0400

    Add background image to login page

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,9 @@
-
+import bg from '../public/oldMain.jpg'
 export default function Login() {
   return (
-    <div>login</div>
+    <div className="min-w-screen min-h-screen bg-no-repeat bg-cover bg-center relative" style={{backgroundImage: `url(${bg.src})`}}>
+      <div className="absolute bg-gradient-to-b from-green-500 to-green-400 opacity-75 inset-0 z-0">
+      </div>
+    </div>
   )
 }
