bf4ef809fd2fa135c1c887ae62209e13d21c8fed
Patrick <patrickducusin2@gmail.com>
2022-10-02 23:38:47 -0400

    Change directory name for component to components

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import styles from '../styles/Home.module.css'
-import ProtectedRoute from '../component/ProtectedRoute'
+import ProtectedRoute from '../components/ProtectedRoute'
 
 export default function Home() {
   return (
