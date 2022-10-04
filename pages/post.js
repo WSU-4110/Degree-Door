47b77ed5e7d72127a9f487931b8fbe9b41cb8951
Majeda Ullah <majedaullah@gmail.com>
2022-10-04 16:33:24 -0400

    Create submit and change functions for post page

--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,47 @@
-import React from 'react'
+import { useState } from 'react'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '../firebase'
 
 export default function Post() {
+  const [formData, setFormData] = useState({
+    course: "",
+    pros: "",
+    cons: "",
+  })
+
+  function handleChange(event) {
+    const { name, value } = event.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const reviewsRef = collection(db, "Reviews")
+    const reviewData = {
+      timeStamp: serverTimestamp(),
+      course: formData.course,
+      pros: formData.pros,
+      cons: formData.cons
+    }
+    await addDoc(reviewsRef, reviewData)
+  }
+
   return (
     <div className="form-wrapper min-h-screen bg-white rounded shadow-md px-6 py-8 flex items-center justify-center">
-      <form className="flex flex-col w-full">
+      <form className="flex flex-col w-full" onSubmit={handleSubmit}>
         <div className="flex flex-col">
             <label>Course Name:</label>
             <textarea
               className="overflow-auto border border-slate-400 border-solid rounded"
-              placeholder="Your review"
-              name="review"
+              placeholder="Course name"
+              name="course"
+              value={formData.course}
+              onChange={handleChange}
             />
         </div>
             
@@ -17,8 +49,10 @@ export default function Post() {
             <label>Pros:</label>
             <textarea
               className="overflow-auto border border-slate-400 border-solid rounded"
-              placeholder="Your review"
-              name="review"
+              placeholder="Pros"
+              name="pros"
+              value={formData.pros}
+              onChange={handleChange}
             />
         </div>
 
@@ -27,8 +61,10 @@ export default function Post() {
           <label>Cons:</label>
           <textarea
             className="overflow-auto border border-slate-400 border-solid rounded"
-            placeholder="Your review"
-            name="review"
+            placeholder="Cons"
+            name="cons"
+            value={formData.cons}
+            onChange={handleChange}
           />
         </div>
 
