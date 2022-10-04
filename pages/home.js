68d1d47b998d4b221632922560e1f0798b46ce55
ZwarA01 <zwarahmed01@gmail.com>
2022-10-04 17:14:53 -0400

    Finished mock home page

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,13 @@
 import Navbar from '../components/Navbar'
+import bg from '../public/oldMain.jpg'
 
 export default function Home() {
-    return(
-        <>
-            <Navbar/>
-            
-        </>
-    )
+  return(
+    <>
+      <Navbar/>
+      <div>
+        <img src='homepage2.png'/>
+      </div>
+    </>
+  )
 }
