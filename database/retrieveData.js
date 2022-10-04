5df81f34711f3eae3504e445767cf10c4f662ce4
Grey Slatina <kslatina@outlook.com>
2022-10-04 16:07:23 -0400

    Created Retrieve Data function

--- database/retrieveData.js
+++ database/retrieveData.js
@@ -1,13 +1,12 @@
-//Delete this comment Grey, also Hello!
-//import { auth, db } from '../firebase'
+import { auth, db } from '../firebase'
 
-//const colRef = collection(db, `Users/${context.query.id}`);
-//const docRef = doc(db, `Users/${context.query.id}`);
+const colRef = collection(db, `Users/${context.query.id}`);
+const docRef = doc(db, `Users/${context.query.id}`);
   
-//// Prep the messages on the server by making a query to all the documents
-//// in the messages subcollection. Order by the most recent message.
+// Prep the messages on the server by making a query to all the documents
+// in the messages subcollection. Order by the most recent message.
 
-//const UserQuery = query(colRef);
+const UserQuery = query(colRef);
 
-//// Get a QuerySnapshot of all the messages.
-//const UsersDocs = await getDocs(UsersQuery);
+// Get a QuerySnapshot of all the messages.
+const UsersDocs = await getDocs(UsersQuery);
