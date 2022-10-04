e34cceed8c4e14d15523cb7cbb1401ffe2e5c720
Ali <he3723@wayne.edu>
2022-10-04 15:05:02 -0400

    Tiny logo added to branding and button styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,20 +3,27 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return(
-    <nav className="flex justify-between h-12 items-center" bg="light" variant="light">
-      <div className="navbar-brand ml-4">
-        <Link href="/">Degree Door</Link>
+    <nav className="flex justify-between h-16 items-center bg-[#67A25B] text-white" bg="light" variant="light">
+      <div className="navbar-brand flex ml-4">
+        <Link href="/">Degree Door </Link>
+        <img src="https://i.imgur.com/wvQkgzH.png"></img>
       </div>
       <div className="navbar-links flex gap-4 mr-4">
+      <button class="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
         <a>
-          <Link href="/">Home</Link>
+          <Link href="/home">Home</Link>
         </a>
+      </button>
+      <button class="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
         <a>
           <Link href="/register">Register</Link>
         </a>
+      </button>
+      <button class="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
         <a>
           <Link href="/login">Login</Link>
         </a>
+      </button>
       </div>
     </nav>
   )
