11c00bdcf76c3953e0a067b0162190a9e1e87a05
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-04 21:14:13 -0400

    Make Navbar component map over link prop

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,6 @@
-//import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import Link from 'next/link'
 
-export default function Navbar() {
+export default function Navbar({ links }) {
   return(
     <nav className="flex justify-between h-16 items-center bg-[#67A25B] text-white" bg="light" variant="light">
       <div className="navbar-brand flex ml-4">
@@ -9,15 +8,13 @@ export default function Navbar() {
         <img src="https://i.imgur.com/wvQkgzH.png"></img>
       </div>
       <div className="navbar-links flex gap-4 mr-4">
-        <button className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
-          <Link href="/">Home</Link>
-        </button>
-        <button className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
-          <Link href="/register">Register</Link>
-        </button>
-        <button className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
-          <Link href="/login">Login</Link>
-        </button>
+        {/* We will map each link in the links array and map a
+          different link to the Link component's href attribute */}
+        {links.map((link, index) => (
+          <button key={index} className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+            <Link href={link.route}>{link.name}</Link>
+          </button>
+        ))}
       </div>
     </nav>
   )
