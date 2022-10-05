29606bb0206395cff2b22afb686cfb1573558bd0
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-04 21:23:38 -0400

    Include link prop for login and register page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function Navbar({ links }) {
+export default function Navbar() {
   return(
     <nav className="flex justify-between h-16 items-center bg-[#67A25B] text-white" bg="light" variant="light">
       <div className="navbar-brand flex ml-4">
@@ -8,13 +8,15 @@ export default function Navbar({ links }) {
         <img src="https://i.imgur.com/wvQkgzH.png"></img>
       </div>
       <div className="navbar-links flex gap-4 mr-4">
-        {/* We will map each link in the links array and map a
-          different link to the Link component's href attribute */}
-        {links.map((link, index) => (
-          <button key={index} className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
-            <Link href={link.route}>{link.name}</Link>
-          </button>
-        ))}
+        <button className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+          <Link href="/">Home</Link>
+        </button>
+        <button className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+          <Link href="/register">Register</Link>
+        </button>
+        <button className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
+          <Link href="/login">Login</Link>
+        </button>
       </div>
     </nav>
   )
