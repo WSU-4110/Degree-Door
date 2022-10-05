c4b0c20b4149c3d3d428973b72627257d5773b99
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-04 21:43:29 -0400

    For home page, provide a way to get to posts page or sign out user

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import ProtectedRoute from '../components/ProtectedRoute'
 export default function Home() {
   return(
     <ProtectedRoute>
-      <Navbar/>
+      <Navbar links={[{route: "/post", name: "Post Review"}, {route:"/signOut", name: "Sign Out"}]}/>
       <div>
         <img src='homepage2.png'/>
       </div>
