ad6b8a8dda6b78542938f848aebb444a0774a7b3
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-04 21:42:14 -0400

    Conditionally render another button that signs user out

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,15 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { auth } from '../firebase'
 
 export default function Navbar({ links }) {
+  const router = useRouter()
+
+  async function handleClick() {
+    await auth.signOut()
+    router.push("/login")
+  }
+
   return(
     <nav className="flex justify-between h-16 items-center bg-[#67A25B] text-white" bg="light" variant="light">
       <div className="navbar-brand flex ml-4">
@@ -10,7 +19,15 @@ export default function Navbar({ links }) {
       <div className="navbar-links flex gap-4 mr-4">
         {/* We will map each link in the links array and map a
           different link to the Link component's href attribute */}
-        {links.map((link, index) => (
+        {links.map((link, index) => (link.route === "/signOut" ?
+          <button 
+            key={index} 
+            className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
+            onClick={handleClick}
+          >
+            {link.name}
+          </button>
+          :
           <button key={index} className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded">
             <Link href={link.route}>{link.name}</Link>
           </button>
