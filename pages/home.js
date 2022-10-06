18fa55aaec2229684ef47bd83a7ca46974230ede
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-06 15:34:09 -0400

    Remove duplicate picture

--- pages/home.js
+++ pages/home.js
@@ -9,10 +9,6 @@ export default function Home() {
         <img src='homepage2.png' alt='waynestate-banner'/>
         <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
       </div>
-      <div className="relative">
-        <img src='homepage2.png' alt='waynestate-banner'/>
-        <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
-      </div>
       <div>
         <input className='mx-96 w-1/2 border-2 p-4 text-center' type='text' placeholder='Search...'></input>
         <button className='mx-96 w-1/2 border-2 p-4 text-center'>Search</button>
