1dfde38f4aa559c0a040a8c48dfe8d6c77d60c74
Patrick <patrickducusin2@gmail.com>
2022-10-17 21:59:16 -0400

    Change dynamic route folder for degree page

--- pages/[degreeID]/index.js
+++
@@ -1,8 +0,0 @@
-export default function DegreeHome() {
-  return (
-    <div>This is what the home page for a degree will look like. There is going to be
-      information that will be populated from the database into this home page.
-    </div>
-
-  )
-}
