ff71f5cc370eeb970992af2667f5a4bd4f4ef9da
Patrick <patrickducusin2@gmail.com>
2022-10-19 00:37:38 -0400

    Fix jsx attribute for anchor tag

--- pages/[degreeID]/index.js
+++ pages/[degreeID]/index.js
@@ -20,9 +20,9 @@ export default function DegreeHome({name, description}) {
       <nav className="degree-page-nav w-full py-4 border-t border-b bg-gray-100">
         <div className="w-full flex-grow sm:flex sm:items-center sm:w-auto">
           <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Overview</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Links</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Reviews</a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Overview</a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Links</a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Reviews</a>
           </div>
         </div>
       </nav>
