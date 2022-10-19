b99523d723e0bee523c65053fbd5f8499b88a8db
Patrick <patrickducusin2@gmail.com>
2022-10-19 01:17:45 -0400

    Create reviews page for degree forums

--- pages/[degreeID]/reviews.js
+++ pages/[degreeID]/reviews.js
@@ -1,5 +1,33 @@
+import { jsonEval } from "@firebase/util";
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+
+import Navbar from '../../components/Navbar'
+import { db } from '../../firebase' 
 export default function Reviews() {
   return (
-    <div>Reviews Page</div>
+    <div className="degree-home bg-white font-Karla relative">
+      <Navbar links={[{route: "/", name: "Home"},{route: "/post", name: "Post Review"}, {route:"/signOut", name: "Sign Out"}]}/>
+      <header class="header-wrapper w-full container mx-auto pt-12">
+        <div class="name-description-wrapper flex flex-col items-center py-12">
+          <div class="display-degree-name font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl">
+            Reviews Page
+          </div>
+          <p class="text-lg text-gray-600 text-center">
+            Here you can check out all of the reviews!
+          </p>
+        </div>
+      </header>
+      <nav className="degree-page-nav w-full py-4 border-t border-b bg-gray-100">
+        <div className="w-full flex-grow sm:flex sm:items-center sm:w-auto">
+          <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+            <a href="/cs" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Overview</a>
+            <a href="#" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Links</a>
+            <a href="/cs/reviews" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">Reviews</a>
+          </div>
+        </div>
+      </nav>
+      <div className="text-center">Hello, this is an overview of what the Review page should look like.</div>
+    </div>
   )
 }
+
