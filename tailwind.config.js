48f94138e0e2cddaf230b6cd12cb92c52238671f
Patrick <patrickducusin2@gmail.com>
2022-10-19 00:12:32 -0400

    Import Karla font from google font api

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'Karla': ["Karla", "sans-serif"]
+    },
     extend: {
       boxShadow: {
         'DEFAULT': '0 0 25px rgba(0, 0, 0, 0.2)',
