8905337a3fdf5144fbaf5bfee6c7fb9ee1c52c0d
Patrick <patrickducusin2@gmail.com>
2022-10-21 00:36:46 -0400

    Fix spacing issues with MotionWrapper

--- components/HOC/MotionWrapper.js
+++ components/HOC/MotionWrapper.js
@@ -1,20 +1,19 @@
 import { motion } from 'framer-motion'
 
 /*
-    Higher order component which will wrap each component in a motion.div
-    that will make an appearing animation whenever the section comes
-    into view.
-    
+  Higher order component which will wrap each component in a motion.div
+  that will make an appearing animation whenever the section comes
+  into view.
 */  
 
 export default function MotionWrap({ Component }) {
   return (
     <motion.div
-        // This motion.div will animate each section such that whenever they
-        // come into view, they appear as they are coming into the page.
-        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1]}}
-        transition={{ duration: 0.5 }}
-        className="flex"
+      // This motion.div will animate each section such that whenever they
+      // come into view, they appear as they are coming into the page.
+      whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1]}}
+      transition={{ duration: 0.5 }}
+      className="flex"
     >
       <Component />
     </motion.div>
