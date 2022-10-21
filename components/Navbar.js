51f9c42684fb50620ef4658f6250854ac3c76062
Patrick <patrickducusin2@gmail.com>
2022-10-21 04:02:35 -0400

    Remove signout functionality in Navbar and changed background color

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,7 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { auth } from '../firebase'
 
-export default function Navbar({ links, dropdown }) {
-  const buttonStyle = "bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded "
-  + "hover:scale-90 ease-in-out duration-300 cursor:pointer"
+export default function Navbar({ links, children }) {
 
   const router = useRouter()
 
@@ -14,7 +12,7 @@ export default function Navbar({ links, dropdown }) {
   }
 
   return(
-    <nav className="fixed z-50 w-full top-0 flex justify-between h-16 items-center bg-[#67A25B] text-white shadow" bg="light" variant="light">
+    <nav className="fixed z-50 w-full top-0 flex justify-between h-16 items-center bg-[#242526] text-white shadow font-Inter" bg="light" variant="light">
       <div className="navbar-brand flex ml-4">
         <Link href="/">Degree Door</Link>
         <img src="https://i.imgur.com/wvQkgzH.png"></img>
@@ -22,25 +20,19 @@ export default function Navbar({ links, dropdown }) {
       <div className="navbar-links flex gap-4 mr-4">
         {/* We will map each link in the links array and map a
           different link to the Link component's href attribute */}
-        {links.map((link, index) => (link.route === "/signOut" ?
+        {links.map((link, index) => (
           <button 
             key={index} 
-            className={buttonStyle}
-            onClick={handleClick}
-          >
-            {link.name}
-          </button>
-          :
-          <button 
-            key={index} 
-            className={buttonStyle}
+            className="justify-center rounded-md bg-slate-300 bg-opacity-20 px-4 py-2 text-sm font-medium 
+            text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white 
+            focus-visible:ring-opacity-75 hover:scale-90 ease-in-out duration-300"
           >
             <Link href={link.route}>{link.name}</Link>
           </button>
         ))}
+        {/*We render the dropdown component here*/}
+        {children}
       </div>
-      {/*We render the dropdown component here*/}
-      {dropdown}
     </nav>
   )
 }
