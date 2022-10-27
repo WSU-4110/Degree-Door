b57dd03862a98de3cdd132a89c5c371349a3c595
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-27 18:31:29 -0400

    Add Search Icon using in Searchbar component

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,14 +1,15 @@
+import { BsSearch } from 'react-icons/bs'
 export default function Searchbar() {
   return (
-    <form className="flex items-center justify-center relative z-[2] text-gray-600 mt-[-20px]">
+    <form className="flex items-center justify-center relative z-[2] text-gray-600 mt-[-1.5rem]">
       <input 
         type="search" 
         name="search" 
         placeholder="Search" 
         className="bg-white border-2 border-neutral-600 w-6/12
-        max-w-xl h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" 
+        max-w-xl h-12 px-5 pr-10 rounded-full text-sm focus:outline-none" 
       />
-      <button type="submit" className="absolute right-0 top-0 mt-3 mr-4"></button>
+      <BsSearch className="text-black relative left-[-2.5rem]"/>
     </form>
   )
 }
