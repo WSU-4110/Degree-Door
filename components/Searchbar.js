68e6d661efd8d40d89713ef2e86eb78aa4c1c83f
PattyCoding123 <patrickducusin2@gmail.com>
2022-10-27 18:02:25 -0400

    Style basic Searchbar UI

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,5 +1,14 @@
 export default function Searchbar() {
   return (
-    <div></div>
+    <form className="flex items-center justify-center relative z-[2] text-gray-600 mt-[-20px]">
+      <input 
+        type="search" 
+        name="search" 
+        placeholder="Search" 
+        className="bg-white border-2 border-neutral-600 w-6/12
+        max-w-xl h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" 
+      />
+      <button type="submit" className="absolute right-0 top-0 mt-3 mr-4"></button>
+    </form>
   )
 }
