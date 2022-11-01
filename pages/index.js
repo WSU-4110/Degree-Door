1c42ab1071a743bbfcb44858071ed435ea32e589
ZwarA01 <zwarahmed01@gmail.com>
2022-11-01 19:34:39 -0400

    adding search bar to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import { collection, query, where, getDocs } from "firebase/firestore";
+
 import Dropdown from '../components/Dropdown'
 import Navbar from '../components/Navbar'
 import Searchbar from '../components/Searchbar'
 import ProtectedRoute from '../components/HOC/ProtectedRoute'
+import { db } from '../firebase'
 
-export default function Home() {
+export default function Home({degreeDocs}) {
+
   return(
     <ProtectedRoute>
       <Navbar links={[{route: "/cs", name: "Computer Science"}, {route: "/post", name: "Post Review"}]}>
@@ -13,7 +17,7 @@ export default function Home() {
         <img src='homepage2.png' alt='waynestate-banner'/>
         <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
       </div>
-      <Searchbar />
+      <Searchbar degrees={degreeDocs}/>
       <div className='font-mono mt-80 ml-10'>
         <h1>Degrees</h1>
       </div>
@@ -36,4 +40,21 @@ export default function Home() {
       </div>
     </ProtectedRoute>
   )
+}
+
+export async function getServerSideProps() {
+  const collectionRef = query(collection(db, "Degrees")); // Create collection reference
+  const degreesSnapshot = await getDocs(collectionRef); // Get document snapshots from firestore
+
+  // Get both degree name and description from the document.
+  const degreesData = degreesSnapshot.docs.map((doc) => ({
+    link: doc.id,
+    name: doc.data().degreeName
+  }))
+  // Return server side props
+  return {
+    props: { 
+      degreeDocs: degreesData,
+    }
+  };
 }
