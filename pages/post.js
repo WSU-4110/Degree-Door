61c071ce337c47810d8cd8d772a69df6681d2058
Majeda Ullah <majedaullah@gmail.com>
2022-11-01 20:28:32 -0400

    Improve UI of post page

--- pages/post.js
+++ pages/post.js
@@ -38,52 +38,56 @@ export default function Post() {
 
   return (
     <ProtectedRoute>
-      <div className="form-wrapper min-h-screen bg-white rounded shadow-md px-6 py-8 flex items-center justify-center">
-        <form className="flex flex-col w-full" onSubmit={handleSubmit}>
-          <div className="flex flex-col">
-            <label>Course Name:</label>
-            <textarea
-              className="overflow-auto border border-slate-400 border-solid rounded"
-              placeholder="Course name"
-              name="course"
-              value={formData.course}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Pros:</label>
-            <textarea
-              className="overflow-auto border border-slate-400 border-solid rounded"
-              placeholder="Pros"
-              name="pros"
-              value={formData.pros}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label>Cons:</label>
-            <textarea
-              className="overflow-auto border border-slate-400 border-solid rounded"
-              placeholder="Cons"
-              name="cons"
-              value={formData.cons}
-              onChange={handleChange}
-            />
-          </div>
-          <button className="submit-button bg-green-900 hover:scale-90 duration-300
-          text-white rounded-full h-10 w-20 mt-6 p-2 cursor-pointer justify-between items-center"
-            type="submit">
-            SUBMIT
-          </button>
-          <Link href="/">
-            <button className="submit-button bg-green-900 hover:scale-90 duration-300
-            text-white rounded-full h-10 w-20 mt-6 p-2 cursor-pointer justify-between items-center"
-              type="button"
-            >
-              HOME
-            </button>
-          </Link>
-        </form>
+      <div class="box-border h-max-[100vh] w-30 p-20"> 
+        <div className="form-wrapper w-1/2 m-auto placeholder:p-4 border-4 bg-gradient-to-b from-gray-200 to-gray-100 opacity-75 inset-0 z-0 bg-white rounded shadow-md px-6 py-8 flex items-center justify-center">
+          <form className="flex flex-col w-full gap-10" onSubmit={handleSubmit}>
+            <div className="flex flex-col">
+              <label><b>COURSE</b></label>
+              <textarea
+                className="overflow-auto border border-slate-400 border-solid rounded p-2"
+                placeholder="Course name"
+                name="course"
+                value={formData.course}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label><b>PROS</b></label>
+              <textarea
+                className="overflow-auto border border-slate-400 border-solid rounded p-2"
+                placeholder="List the positives of this course"
+                name="pros"
+                value={formData.pros}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label><b>CONS</b></label>
+              <textarea
+                className="overflow-auto border border-slate-400 border-solid rounded p-2"
+                placeholder="List the negatives of this course"
+                name="cons"
+                value={formData.cons}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <div class="p-2 flex">
+                <div class="w-1/2"></div>
+                <div class="w-1/2 flex justify-end">
+                  <Link href="/">
+                    <button type="button" class="bg-black hover:scale-90 duration-300 text-white p-2 rounded text-xs w-auto">
+                      CANCEL
+                    </button>
+                  </Link>
+                  <button type="submit" class="bg-green-900 hover:scale-90 duration-300 text-white p-2 ml-6 rounded text-xs w-auto">
+                    SUBMIT
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </ProtectedRoute>
   )
