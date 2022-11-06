e99377428c344b02b4aa1b2c6159bfc29f4ec622
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-06 02:17:28 -0500

    Wrap HomePage component with NavbarHOC

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 
 import Dropdown from '../components/Dropdown'
 import Navbar from '../components/Navbar'
+import NavbarHOC from "../components/HOC/NavbarHOC";
 import Searchbar from '../components/Searchbar'
 import ProtectedRoute from '../components/HOC/ProtectedRoute'
 import { db } from '../firebase'
@@ -10,34 +11,33 @@ export default function Home({degreeDocs}) {
 
   return(
     <ProtectedRoute>
-      <Navbar links={[{route: "/cs", name: "Computer Science"}, {route: "/post", name: "Post Review"}]}>
-        <Dropdown />
-      </Navbar>
-      <div className="relative">
-        <img src='homepage2.png' alt='waynestate-banner'/>
-        <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
-      </div>
-      <Searchbar degrees={degreeDocs}/>
-      <div className='font-mono mt-80 ml-10'>
-        <h1>Degrees</h1>
-      </div>
-      <div className='after:: content-none clear-both table mx-auto w-full h-full'>
-        <div className='float-left w-1/5 h-20 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+      <NavbarHOC authenticated={true}>
+        <div className="relative">
+          <img src='homepage2.png' alt='waynestate-banner'/>
+          <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
         </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+        <Searchbar degrees={degreeDocs}/>
+        <div className='font-mono mt-80 ml-10'>
+          <h1>Degrees</h1>
         </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+        <div className='after:: content-none clear-both table mx-auto w-full h-full'>
+          <div className='float-left w-1/5 h-20 p-5px'>
+            <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+          </div>
+          <div className='float-left w-1/5 p-5px'>
+            <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+          </div>
+          <div className='float-left w-1/5 p-5px'>
+            <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+          </div>
+          <div className='float-left w-1/5 p-5px'>
+            <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+          </div>
+          <div className='float-left w-1/5 p-5px'>
+            <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
+          </div>
         </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
-      </div>
+      </NavbarHOC>
     </ProtectedRoute>
   )
 }
