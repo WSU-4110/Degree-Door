9a9230563e8f5d96206d8c055f083a40d21851cd
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-08 15:58:58 -0500

    Pull favorite degrees from firebase

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { collection, query, getDocs } from "firebase/firestore";
 import { useRouter } from 'next/router'
 
@@ -7,7 +8,7 @@ import Searchbar from '../components/Searchbar'
 import ProtectedRoute from '../components/HOC/ProtectedRoute'
 import { db } from '../firebase'
 
-export default function Home({degreeDocs}) {
+export default function Home({degreeDocs, favDegrees}) {
   const router = useRouter()
 
   return(
@@ -23,33 +24,36 @@ export default function Home({degreeDocs}) {
       <div className='font-mono mt-80 ml-10'>
         <h1>Degrees</h1>
       </div>
+      
       <div className='after:: content-none clear-both table mx-auto w-full h-full'>
-        <div className='float-left w-1/5 h-20 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
-        <div className='float-left w-1/5 p-5px'>
-        <img src='https://thumbs.dreamstime.com/b/computer-science-isolated-shiny-blue-square-button-computer-science-shiny-blue-square-button-143199787.jpg' placeholder='computer-science-placeholder-img'></img>
-        </div>
+        {favDegrees.map((degree, index) => (
+          <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
+            <div className='float-left w-1/5 h-20 p-5px'>
+              {degree.name}
+            </div>
+          </Link>
+
+        ))}
       </div>
     </ProtectedRoute>
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
   const collectionRef = query(collection(db, "Degrees")); // Create collection reference
   const degreesSnapshot = await getDocs(collectionRef); // Get document snapshots from firestore
 
+  const favoritesRef = query(collection(db,`Users/${context.query.userID}/Favorites`))
+  const favoritesSnapshot = await getDocs(favoritesRef)
+
   // Get both degree name and description from the document.
   const degreesData = degreesSnapshot.docs.map((doc) => ({
+    link: doc.id,
+    name: doc.data().degreeName
+  }))
+
+
+  const favDegreesData = favoritesSnapshot.docs.map((doc) => ({
     link: doc.id,
     name: doc.data().degreeName
   }))
@@ -57,6 +61,7 @@ export async function getServerSideProps() {
   return {
     props: { 
       degreeDocs: degreesData,
+      favDegrees: favDegreesData
     }
   };
 }
