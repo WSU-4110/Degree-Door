237f82d52ace00bd37f2399f05e76dccd7ba4e9c
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-08 11:32:34 -0500

    Create the states for the FavoritesDialog

--- components/FavoritesDialog.js
+++ components/FavoritesDialog.js
@@ -3,11 +3,15 @@ import {  Button, Dialog, DialogActions, DialogContent,
 DialogContentText, DialogTitle, useMediaQuery, useTheme } from '@mui/material'
 import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
 
-export default function FavoritesDialog() {
-  const [open, setOpen] = useState(false);
+export default function FavoritesDialog({ handleClick, initFavState}) {
+  const [open, setOpen] = useState(false)
+  const [isFavorite, setIsFavorite] = useState({initFavState})
+  const theme = useTheme()
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
 
   return (
     <div className="dialog-wrapper">
+      
     </div>
   )
 }
