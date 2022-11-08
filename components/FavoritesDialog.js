ea2260b8a1c1459c4c7855bceda79be85943562f
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-08 11:30:47 -0500

    Create imports from material UI

--- components/FavoritesDialog.js
+++ components/FavoritesDialog.js
@@ -1,5 +1,13 @@
+import { useState } from 'react'
+import {  Button, Dialog, DialogActions, DialogContent,
+DialogContentText, DialogTitle, useMediaQuery, useTheme } from '@mui/material'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai'
+
 export default function FavoritesDialog() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>FavoritesDialog</div>
+    <div className="dialog-wrapper">
+    </div>
   )
 }
