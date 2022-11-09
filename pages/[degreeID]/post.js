fa487e48449494a88cc87f05d9dbbea5af8245dd
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-09 16:53:43 -0500

    Include a form validation in the post page

--- pages/[degreeID]/post.js
+++ pages/[degreeID]/post.js
@@ -1,17 +1,20 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 
-import Dropdown from '../components/Dropdown'
-import Navbar from '../components/Navbar'
-import ProtectedRoute from '../components/HOC/ProtectedRoute'
-import { useAuthContext } from '../context/AuthContext'
+import Dropdown from '../../components/Dropdown'
+import Navbar from '../../components/Navbar'
+import ProtectedRoute from '../../components/HOC/ProtectedRoute'
+import { useAuthContext } from '../../context/AuthContext'
 
 export default function Post() {
+  const router = useRouter()
   const { db } = useAuthContext() // Gain db from AuthContext
   const [open, setOpen] = useState(false)
   const [openSuccess, setOpenSuccess] = useState(false)
   const [openCancellation, setOpenCancellation] = useState(false)
+  const [openError, setOpenError] = useState(false)
 
   const [formData, setFormData] = useState({
     course: "",
@@ -29,13 +32,21 @@ export default function Post() {
     })
   }
 
-  async function handleSubmit(event) {
+  function validateForm() {
+    if (formData.course === "" || formData.pros === "" || formData.cons === "") {
+      setOpenError(true)
+    } else {
+      setOpen(true);
+    }
+  }
+
+  async function handleSubmit() {
     const reviewsRef = collection(db, "Degrees/cs/Reviews")
     const reviewData = {
       timeStamp: serverTimestamp(),
       course: formData.course,
       pros: formData.pros,
-      cons: formData.cons
+      cons: formData.cons,
     }
     await addDoc(reviewsRef, reviewData)
     setFormData({
@@ -50,11 +61,43 @@ export default function Post() {
 
   return (
     <ProtectedRoute>
-       <Navbar>
+      <Navbar user={router.query.userID}>
         <Dropdown />
       </Navbar>
       <div className="box-border h-max-[100vh] w-30 p-20">
       
+       {/* begin dialog box confirmation message for cancel button */}
+       {openError && 
+       <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">WARNING</h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">Please fill out all fields before submitting</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button 
+                  type="button" 
+                  className="mt-3 inline-flex w-auto justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => setOpenError(false)}
+                >
+                  Ok
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>}
+      {/* end cancel dialog box */}
+
       {/* begin dialog box confirmation message for cancel button */}
       {openCancellation && <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
@@ -72,14 +115,14 @@ export default function Post() {
                 </div>
               </div>
               <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <Link href="/">
+              <Link href={{pathname: `/${router.query.degreeID}/`, query: {userID: router.query.userID}}}>
                 <button 
                   type="button" 
                   className="inline-flex w-auto justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                 >
                   Leave this Page
                 </button>
-                </Link>
+              </Link>
                 <button 
                   type="button" 
                   className="mt-3 inline-flex w-auto justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
@@ -192,7 +235,7 @@ export default function Post() {
                   <button 
                     type="button" 
                     className="inline-flex w-auto justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setOpen(true)}
+                    onClick={validateForm}
                   >
                     SUBMIT
                   </button>
