881fedca42db1298e077907eb1a1b3aa29b4cc6d
Majeda Ullah <majedaullah@gmail.com>
2022-11-09 13:55:55 -0500

    Apply cancelation dialog box

--- pages/post.js
+++ pages/post.js
@@ -11,6 +11,7 @@ export default function Post() {
   const { db } = useAuthContext() // Gain db from AuthContext
   const [open, setOpen] = useState(false)
   const [openSuccess, setOpenSuccess] = useState(false)
+  const [openCancellation, setOpenCancellation] = useState(false)
 
   const [formData, setFormData] = useState({
     course: "",
@@ -44,6 +45,7 @@ export default function Post() {
     })
     setOpen(false)
     setOpenSuccess(true)
+    setOpenCancellation(false)
   }
 
   return (
@@ -52,8 +54,47 @@ export default function Post() {
         <Dropdown />
       </Navbar>
       <div className="box-border h-max-[100vh] w-30 p-20">
+      
+      {/* begin dialog box confirmation message for cancel button */}
+      {openCancellation && <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900" id="modal-title">WARNING</h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">You have unsaved changes. Are you sure you want to leave this page?</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <Link href="/">
+                <button 
+                  type="button" 
+                  className="inline-flex w-auto justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  Leave this Page
+                </button>
+                </Link>
+                <button 
+                  type="button" 
+                  className="mt-3 inline-flex w-auto justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => setOpenCancellation(false)}
+                >
+                  Stay on this Page
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>}
+      {/* end cancel dialog box */}
 
-      {/* add dialog box confirmation message for submit button */}
+      {/* begin dialog box confirmation message for submit button */}
       {open && <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -89,9 +130,9 @@ export default function Post() {
           </div>
         </div>
       </div>}
-      {/* end dialog box */}
+      {/* end submit dialog box */}
 
-      {/* add success alert */}
+      {/* begin success alert */}
       {openSuccess && <div id="alert" className="flex w-[700px] ml-[280px] p-4 mb-4 bg-green-100 rounded-lg dark:bg-green-200" role="alert">
         <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-700 dark:text-green-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
         <div className="ml-3 text-sm font-medium text-green-700 dark:text-green-800">
@@ -105,7 +146,7 @@ export default function Post() {
       </div>}
       {/* end success alert */}
 
-        <div className="form-wrapper w-1/2 m-auto mt-8 placeholder:p-4 border-4 bg-gradient-to-b from-gray-200 to-gray-100 opacity-75 inset-0 z-0 bg-white rounded shadow-md px-6 py-8 flex items-center justify-center">
+        <div className="form-wrapper w-1/2 m-auto mt-8 placeholder:p-4 pl-10 pr-10 border-4 bg-gradient-to-b from-gray-200 to-gray-100 opacity-75 inset-0 z-0 bg-white rounded shadow-md px-6 py-8 flex items-center justify-center">
           <form className="flex flex-col w-full gap-10" onSubmit={handleSubmit}>
             <div className="flex flex-col">
               <label><b>COURSE</b></label>
@@ -141,11 +182,13 @@ export default function Post() {
               <div className="p-2 flex">
                 <div className="w-1/2"></div>
                 <div className="w-1/2 flex justify-end">
-                  <Link href="/">
-                    <button type="button" className="mt-3 inline-flex w-auto justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                    <button 
+                    type="button" 
+                    className="mt-3 inline-flex w-auto justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => setOpenCancellation(true)}
+                    >
                       CANCEL
                     </button>
-                  </Link>
                   <button 
                     type="button" 
                     className="inline-flex w-auto justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
