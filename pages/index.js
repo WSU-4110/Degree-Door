beb8dcf7cf723462671290062aa31834dba6288e
Ali <he3723@wayne.edu>
2022-11-10 13:12:24 -0500

    Added div classes to make cards for favorites

--- pages/index.js
+++ pages/index.js
@@ -21,20 +21,34 @@ export default function Home({degreeDocs, favDegrees}) {
         <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
       </div>
       <Searchbar degrees={degreeDocs} user={router.query.userID}/>
-      <div className='font-mono mt-80 ml-10'>
-        <h1>Degrees</h1>
-      </div>
-      
-      <div className='after:: content-none clear-both table mx-auto w-full h-full'>
-        {favDegrees.map((degree, index) => (
-          <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
-            <div className='float-left w-1/5 h-20 p-5px'>
-              {degree.name}
-            </div>
-          </Link>
 
-        ))}
+      <div className='font-mono mt-80 ml-10'>
+        <div class="flex flex-col">
+          <h2 class="mb-4 text-2xl font-bold">Degrees</h2>
+             <div class="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
+              <div className='after:: content-none clear-both table mx-auto w-full h-full'>
+                {favDegrees && favDegrees.map((degree, index) => (
+                <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
+                <div class="flex items-start rounded-xl bg-white p-4 shadow-lg">
+                  <div class="flex h-14 w-14   items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                    </svg>
+                    </div>
+                    <div>
+                    <br></br>
+                    <p class="text-align: right-5">{degree.name}</p>
+                    Description: 
+                    <p class="mt-2 text-sm text-gray-500">test test test</p>
+                  </div>
+                  </div>
+              </Link>
+          ))}
+          </div>
+        </div>
       </div>
+    </div>
     </ProtectedRoute>
   )
 }
