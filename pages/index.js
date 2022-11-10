d70eb7ad14e042b0a989f83731a64baef8a067f0
Ali <he3723@wayne.edu>
2022-11-10 13:45:20 -0500

    Cleaned up code

--- pages/index.js
+++ pages/index.js
@@ -23,20 +23,19 @@ export default function Home({degreeDocs, favDegrees}) {
       <Searchbar degrees={degreeDocs} user={router.query.userID}/>
 
       <div className='font-mono mt-80 ml-10'>
-        <div class="flex flex-col">
-          <h2 class="mb-4 text-2xl font-bold">Favorited Degrees</h2>
-             <div class="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-
-              <div className='after:: content-none clear-both table mx-auto w-full h-full'>
-                {favDegrees && favDegrees.map((degree, index) => (
-                <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
-                <div class="flex items-start rounded-xl bg-white p-4 shadow-lg">
-                  <div class="flex h-14 w-14   items-center justify-center rounded-full border border-blue-100 bg-blue-50">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                    </svg>
-                    </div>
-                    <div>
+        <div className="flex flex-col">
+          <h2 className="mb-4 text-2xl font-bold">Favorited Degrees</h2>
+          <div className="mt-4 grid grid-col-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className='after:: content-none clear-both table mx-auto w-full h-full'>
+              {favDegrees && favDegrees.map((degree, index) => (
+              <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
+              <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+                <div className="flex h-14 w-14   items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                  </svg>
+                </div>
+                  <div>
                     <br></br>
                     <p class="text-align: right-5 text-center">{degree.name}</p>
                     Info: 
@@ -44,7 +43,7 @@ export default function Home({degreeDocs, favDegrees}) {
                     <br></br>
                     <p class="text-center"> x </p>
                   </div>
-                  </div>
+                </div>
               </Link>
           ))}
           </div>
