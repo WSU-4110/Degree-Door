c8b298c42affb9017051d9233a49df4a7c8953a8
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-10 21:11:14 -0500

    Add key to components that we map over

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({degreeDocs, favDegrees}) {
           <h2 className="mb-4 text-2xl font-bold">Favorited Degrees</h2>   
           <div className='flex gap-4 mx-auto w-full h-full'>
             {favDegrees && favDegrees.map((degree, index) => (
-            <div className="mt-4 flex items-start rounded-xl bg-white p-4 shadow-lg">
+            <div key={index} className="mt-4 flex items-start rounded-xl bg-white p-4 shadow-lg">
               <div className="flex h-10 w-10   items-center justify-center rounded-full border border-blue-100 bg-blue-50">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
