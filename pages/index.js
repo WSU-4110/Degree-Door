688d02d42aac7aa30b4c906bd71ca39f06e24688
PattyCoding123 <patrickducusin2@gmail.com>
2022-11-14 14:10:20 -0500

    Standardize stylings in Home page favorites

--- pages/index.js
+++ pages/index.js
@@ -26,22 +26,20 @@ export default function Home({degreeDocs, favDegrees}) {
           <h2 className="mb-4 text-2xl font-bold">Favorited Degrees</h2>   
           <div className='flex gap-4 mx-auto w-full h-full'>
             {favDegrees && favDegrees.map((degree, index) => (
-            <div key={index} className="mt-4 w-1/3 flex items-start rounded-xl bg-white p-4 shadow-lg">
-              <div className="flex h-10 w-10   items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+            <div key={index} className="mt-4 w-1/3 flex flex-col gap-4 items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                 </svg>
               </div>
-              <div>
-              <br/>
+              <div className="w-full h-full">
                 <p class="text-align: right-5 text-center">{degree.name}</p>
                 Info: 
                 <p class="mt-2 text-sm text-gray-500">{degree.description}</p>
-                <br></br>
-                <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
-                  <button type="button" class="inline-grid px-6 py-2.5 bg-lime-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-lime-700 hover:shadow-lg focus:bg-lime-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Visit</button>
-                </Link>
               </div>
+              <Link href={{pathname: `/${degree.link}`, query: {userID: router.query.userID}}} key={index}>
+                <button type="button" class="inline-grid px-6 py-2.5 bg-lime-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-lime-700 hover:shadow-lg focus:bg-lime-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Visit</button>
+              </Link>
             </div>
             ))}
           </div>
