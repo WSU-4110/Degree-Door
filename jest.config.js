230eb25aece11222946aefd798c5e2da976c06b2
Patrick <patrickducusin2@gmail.com>
2022-11-15 01:40:10 -0500

    Setup the dotenv/config file

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,14 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const customJestConfig = {
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  setupFiles: ['dotenv/config'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/functions'],
+  transform: { '\\.m?[jt]sx?$': ['babel-jest', { presets: ['next/babel'] }] },
+  transformIgnorePatterns: ['/node_modules/(?!(firebase)/)', '^.+\\.module\\.(css|sass|scss)$'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
