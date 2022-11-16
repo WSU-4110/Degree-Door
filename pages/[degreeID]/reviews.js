68d2fa03ae85e1a347c5e8319c68e4b0159ec030
ZwarA01 <zwarahmed01@gmail.com>
2022-11-15 19:53:31 -0500

    Deleted unneeded comments in reviews page

--- pages/[degreeID]/reviews.js
+++ pages/[degreeID]/reviews.js
@@ -24,14 +24,6 @@ export default function Reviews({reviews, initFavState}) {
     setToBeDeleted(id)
     setOpenDeletion(true)
   }
-  
-  // function validateForm() {
-  //   if (formData.course === "" || formData.pros === "" || formData.cons === "") {
-  //     setOpenError(true)
-  //   } else {
-  //     setOpen(true);
-  //   }
-  // }
 
   return (
     <div className="degree-home bg-white font-Inter relative">
@@ -140,7 +132,6 @@ export default function Reviews({reviews, initFavState}) {
                 onClick={() => confirmDelete(review.id)}
                 className="cursor-pointer"
               >
-                {/* onClick={validateForm} */}
                 <svg onClick={() => confirmDelete(review.id)} className="fill-current text-gray-700 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                   <path onClick={() => confirmDelete(review.id)} d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"></path>
                 </svg>
