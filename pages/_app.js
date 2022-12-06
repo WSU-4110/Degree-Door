996b0b049155477db717f8524cf34db14e3533f1
ZwarA01 <zwarahmed01@gmail.com>
2022-12-06 15:52:47 -0500

    Update app.js so i can test FAQ page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import AuthContext from '../context/AuthContext'
+import FAQs from './FAQpage'
 
 function MyApp({ Component, pageProps }) {
+  return (<FAQs />)
   return (
     <AuthContext>
       <Component {...pageProps} />
