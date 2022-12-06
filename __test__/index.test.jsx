f85c77751bd4ebf4e7789326d1ae0e39431b94ec
Patrick <patrickducusin2@gmail.com>
2022-12-05 23:51:05 -0500

    Add Majeda's unit testing

--- __test__/index.test.jsx
+++ __test__/index.test.jsx
@@ -86,3 +86,44 @@ describe('Login UI', () => {
     expect(forgotPasswordLink).toBeInTheDocument()
   })
 })
+
+// Majeda Ullah's Unit Testing
+describe('Create Post UI Testing', () => {
+  beforeEach(() => render(<MockPost />))
+  
+  it('The Create Post page should render a top label that says the name of the degree', () => {
+    const degreeName = screen.getByText("Mock Degree")
+
+    expect(degreeName).toBeInTheDocument()
+  })
+
+  it('The Navbar in Create Post should have a label that says "Overview"', () => {
+    const overviewLabel = screen.getByText("OVERVIEW")
+
+    expect(overviewLabel).toBeInTheDocument()
+  })
+
+  it('The Navbar in Create Post should have a label that says "Reviews"', () => {
+    const reviewLabel = screen.getByText("REVIEWS")
+
+    expect(reviewLabel).toBeInTheDocument()
+  })
+
+  it('The Navbar in Create Post should have a label that says "Post a Review"', () => {
+    const postLabel = screen.getByText("POST A REVIEW")
+
+    expect(postLabel).toBeInTheDocument()
+  })
+
+  it('The form in Create Post should have a button to submit', () => {
+    const submitButton = screen.getByRole('button', {name: /SUBMIT/i})
+
+    expect(submitButton).toBeInTheDocument()
+  })
+
+  it('The form in Create Post should have a button to go back', () => {
+    const goBackButton = screen.getByRole('button', {name: /GO BACK/i})
+
+    expect(goBackButton).toBeInTheDocument()
+  })
+})
