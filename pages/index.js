b1ff15f358cc1661a6d7257ff4d6d3586d2dbbd7
Majeda Ullah <majedaullah@gmail.com>
2022-12-07 16:49:04 -0500

    Changed position of searchbar to be on navbar in searchbar component. Added carousel component to homepage.

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,39 @@ import Footer from '../components/Footer'
 import ProtectedRoute from '../components/HOC/ProtectedRoute'
 import FavoriteDegree from '../components/FavoriteDegree'
 import { db } from '../firebase'
+import Carousel from '../components/Carousel'
 
 export default function Home({degreeDocs, favDegrees}) {
   const router = useRouter()
 
   return(
     <ProtectedRoute>
-      <Navbar user={router.query.userID}>
-        <Dropdown />
-      </Navbar>
-      <div className="relative">
-        <img src='homepage2.png' alt='waynestate-banner'/>
-        <h1 className="absolute text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black font-bold">Degree Door</h1>
+      {/* begin nav bar */}
+      <nav className="bg-[#292828] px-2 py-2.5">
+        <div className="container flex text-[#FFFFFF] justify-between items-center ">
+          <div className="navbar-brand flex">
+            <img className="w-6 h-6 ml-2 text-[#FFFFFF]" src="https://i.imgur.com/PUIKaAn.png"></img><b>egreeDoor</b>
+          </div>
+          <Searchbar degrees={degreeDocs} user={router.query.userID}/>
+          <div className="flex">
+            <Dropdown color="#FFFFFF"/>
+          </div>
+        </div>
+      </nav>
+
+      <div className="relative h-[200px] mb-8 w-auto bg-white">
+        {/* <img src='https://i.imgur.com/FVOB96X.png' className='h-20 w-20'></img> */}
+        <h1 className="absolute text-4xl top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-transparent font-bold bg-clip-text bg-gradient-to-r from-green-700 to-green-900">Welcome to DegreeDoor</h1>
+        <p className="absolute top-[110px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 mb-3 font-light text-center text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+        <p className='absolute top-[180px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black'>
+          <button type="button" class="text-white bg-green-900 hover:bg-green-800 font-medium rounded-md text-sm px-4 py-2 text-center inline-flex">
+            Learn More
+            <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+        </p>
       </div>
-      <Searchbar degrees={degreeDocs} user={router.query.userID}/>
+      <Carousel degrees={degreeDocs} user={router.query.userID}/>
+
       <div className='font-mono mt-80 mb-10 mx-4'>
         <div className="flex flex-col">
           <h2 className="mb-4 text-2xl font-bold">Favorited Degrees</h2>   
