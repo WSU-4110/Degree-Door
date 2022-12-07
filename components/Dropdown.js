92697eb5e56bf1f08bf8f2e56688991b84492aa1
Majeda Ullah <majedaullah@gmail.com>
2022-12-07 16:51:55 -0500

    Fixed menu popup in dropdown component.

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -45,7 +45,7 @@ export default function Dropdown({ color }) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Items className="absolute right-0 mt-2 w-40 z-50 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
               <Menu.Item>
                 {({ active }) => (
