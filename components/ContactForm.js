5f74fad2982eb4953ed582946859e56365898e61
Patrick <patrickducusin2@gmail.com>
2022-12-08 01:41:54 -0500

    Make Button Green for contact form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -75,7 +75,7 @@ export default function ContactForm({ setOpenSuccess, userID }) {
       </div>
       <div className="mb-3 pt-0">
         <button
-          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          className="bg-green-500 text-white active:bg-green-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
           type="submit"
         >
           Send a message
