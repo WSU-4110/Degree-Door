e1509451db0c664a79b2df5d6bdc953fef3b62fa
majedau <majedaullah@gmail.com>
2022-12-08 17:16:26 -0500

    Add profile link in dropdown component

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -49,6 +49,18 @@ export default function Dropdown({ color }) {
         >
           <Menu.Items className="absolute right-0 mt-2 w-40 z-50 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             <div className="px-1 py-1 ">
+            <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-green-800 text-white' : 'text-gray-900'
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    onClick={handleProfileClick}
+                  >
+                    Profile
+                  </button>
+                )}
+              </Menu.Item>
               <Menu.Item>
                 {({ active }) => (
                   <button
