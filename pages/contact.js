6f05e620cbeef22396f4277dfbd324646c6ae17d
PattyCoding123 <patrickducusin2@gmail.com>
2022-12-09 16:56:27 -0500

    Fix contact page unnecessary router.query.id check

--- pages/contact.js
+++ pages/contact.js
@@ -1,54 +1,37 @@
-import Dropdown from "../components/Dropdown"
-import Link from "next/link"
-import ContactForm from "../components/ContactForm"
 import { useRouter } from "next/router"
 import { useState } from "react"
-import { GeneralSuccessDialog } from "../components/Dialogs"
+
+import ContactForm from "../components/ContactForm"
 import Footer from "../components/Footer"
+import { GeneralSuccessDialog } from "../components/Dialogs"
 
 export default function Contact(){
   const [openSuccess, setOpenSuccess] = useState(false)
   const router = useRouter()
+  const { userID } = router.query
   return(
     <>
-    <div className="bg-[#f9f9f9] min-h-screen">
-      {/* begin nav bar */}
-      <nav className="bg-[#292828] border-gray-200 border-b-2 px-2 sm:px-4 py-2.5 dark:bg-gray-900 shadow-md">
-        <div className="container text-white flex flex-wrap justify-between items-center mx-auto">
-          <Link href={router.query.userID !== undefined ? {pathname: "/", query: {userID: router.query.userID}} :"/login"}>
-            <div className="navbar-brand cursor-pointer flex">
-              <img className="w-6 h-6 ml-2 text-[#ffffff]" src="https://i.imgur.com/PUIKaAn.png%22%3E"/><b>egreeDoor</b>
-            </div>
-          </Link>
-          <div className='text-white mr-[4.5rem]'>
-            <b>Contact Us</b>
-          </div>
-          <div className="flex md:order-2">
-            {router.query.userID && <Dropdown color="#FFFFFF"/> }
-          </div>
-        </div>
-      </nav>
-      {/* end nav bar */}
-      { openSuccess && <GeneralSuccessDialog setOpenSuccess={setOpenSuccess} message="Message was sent successfully!"/>}
-      {/* end nav bar */}
-      <div className="bg-[#f9f9f9]">
-        <div className="w-6/12 mx-auto rounded mt-10">
-          <div className="bg-white p-10 shadow-sm mb-10">
-            <h3 className="text-lg font-medium text-gray-800 text-center">Contact</h3>
-            <p className="text-sm font-light text-gray-600 my-3 text-center">
-            Email us with any questions or inquiries or call 1-800-DegreeD. We would be happy to answer your questions! 
-            </p>
-            <div className="border-b mb-8" />
-            <div className="py-6">
-            <ContactForm setOpenSuccess={setOpenSuccess}/>
+      <div className="bg-[#f9f9f9] min-h-screen">
+        <NewNavbar pageTitle="Contact Us" userID={userID} />
+        { openSuccess && <GeneralSuccessDialog setOpenSuccess={setOpenSuccess} message="Message was sent successfully!"/>}
+        <div className="bg-[#f9f9f9]">
+          <div className="w-6/12 mx-auto rounded mt-10">
+            <div className="bg-white p-10 shadow-sm mb-10">
+              <h3 className="text-lg font-medium text-gray-800 text-center">Contact</h3>
+              <p className="text-sm font-light text-gray-600 my-3 text-center">
+              Email us with any questions or inquiries or call 1-800-DegreeD. We would be happy to answer your questions! 
+              </p>
+              <div className="border-b mb-8" />
+              <div className="py-6">
+              <ContactForm setOpenSuccess={setOpenSuccess}/>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div className="min-w-full">
-      <Footer userID={router.query.userID !== undefined ? router.query.userID : ""}/>
-    </div>
+      <div className="min-w-full">
+        <Footer userID={userID}/>
+      </div>
     </>
   )
 }
